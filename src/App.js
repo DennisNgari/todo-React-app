a98ebbc44fe41ee7a35779b29528ab533d5d17dc
Dennis Ngari <ngaridennis3@gmail.com>
2021-04-30 22:49:10 +0300

    React todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import "./App.css";
+// Importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  //State stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos]=useState([]);
+
+
+
+
+  // Run once when the App starts
+  useEffect(()=>{
+    getLocalTodos();
+  }, []);
+  // UseEffect Allows us to run a function in two differnt places.
+  // When the user clicks submit and when the user selects/ toggles between completed and uncompleted.
+  useEffect(()=>{
+  filterHandler();
+  saveLocalTodos();
+  }, [todos, status])
+
+  // Functions and events.
+  // You can add this function on the use effect function to avoid the warning on the console.
+  const filterHandler = ()=>{
+    switch(status){
+      case "completed":
+        setFilteredTodos(todos.filter(todo=>todo.completed === true))
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter(todo=>todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos)
+        break;
+    }
+  };
+
+  // Save to local storage 
+  const saveLocalTodos=()=>{
+      localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  
+  //Get saved todos
+  const getLocalTodos = ()=>{
+    if(localStorage.getItem("todos")=== null){
+      localStorage.setItem("todos", JSON.stringify([]));
+    }else{
+      let todoLocal= JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <header>
+      <h1>My Todo List </h1>
+    </header>
+    <Form 
+    inputText = {inputText}
+    todos={todos}
+    setTodos = {setTodos}
+    setInputText={setInputText}
+    setStatus = {setStatus}
+    />
+    <TodoList 
+    filteredTodos = {filteredTodos}
+    setTodos = {setTodos}
+    todos = {todos}/> 
+  </div>
   );
 }
 
 export default App;
+ 
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+//Import components
+import Todo from "./Todo";
+
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            todo={todo}
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            text={todo.text}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
